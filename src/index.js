ebad813d1d6c79a1ec634938c59943a46755f4be
Raymond <lumine2008@hotmail.com>
2026-02-27 23:56:13 +0800

    添加移动端强制显示修复

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './mobile-fix.css';
 import App from './App';
 import MediaPage from './components/MediaPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
